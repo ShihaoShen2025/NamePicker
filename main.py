01ce3212c52aecb058542b63d0fe6b791d03ca4f
LHGS_github <340994706@qq.com>
2025-03-02 12:04:55 +0800

    一个勉强能用的版本

---
+++ main.py
@@ -0,0 +1,36 @@
+import tkinter
+from tkinter import ttk
+import sv_ttk
+import darkdetect
+import random
+
+allowRepeat = False
+names = []
+chosen = []
+with open("names.txt","r",encoding="utf-8") as f:
+    for i in f.readlines():
+        names.append(i.strip("\n"))
+
+def pick():
+    global name,names,allowRepeat,chosen
+    ch = random.choice(names)
+    if not allowRepeat:
+        if len(names) == len(chosen):
+            chosen = []
+        while ch in chosen:
+            ch = random.choice(names)
+        chosen.append(ch)
+    name.config(text=ch)
+
+root = tkinter.Tk()
+root.geometry("200x200")
+root.attributes('-topmost', 'true')
+root.title("随机抽选")
+
+name = ttk.Label(root,text="11111")
+name.place(x=100,y=50,anchor="center")
+button = ttk.Button(root, text="点击以抽选",command=pick)
+button.place(x=100,y=100,anchor="center")
+
+sv_ttk.set_theme(darkdetect.theme())
+root.mainloop()
