626cbb5a501c6ac6646591488f748c334cfe7d46
LHGS_github <340994706@qq.com>
2025-03-08 12:48:49 +0800

    feat:完成只抽男/女功能

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import darkdetect
 import random
 import json
 
+from numpy.ma.core import count
+
 import configgui
 import pandas as pd
 
@@ -13,7 +15,7 @@ VERSION = "1.0.0dev"
 VER_NO = 1
 class App(tkinter.Tk):
     def __init__(self):
-        global allowRepeat,alwaysOnTop,showName
+        global allowRepeat,alwaysOnTop,showName,pref
         allowRepeat = False
         alwaysOnTop = True
         showName = True
@@ -24,26 +26,41 @@ class App(tkinter.Tk):
         self.resizable(False, False)
         self.loadcfg()
         sv_ttk.set_theme(darkdetect.theme())
+        pref = [tkinter.StringVar(), tkinter.IntVar()]
         self.loadname()
         self.createWidget()
     names = []
     chosen = []
     length = 0
+    sexlen = [0,0]
+    sexl = [[],[]]
     def pick(self):
         global allowRepeat,showName
-        chs = random.randint(0,self.length-1)
+        if pref[0].get() != "男女都抽":
+            if pref[0].get() == "只抽男":
+                le = self.sexlen[0]
+                tar = self.sexl[0]
+            else:
+                le = self.sexlen[1]
+                tar = self.sexl[1]
+        else:
+            le = self.length
+            tar = self.names[0]
+
+        chs = random.randint(0, le - 1)
         if not allowRepeat:
-            if len(self.chosen)==self.length:
+            if len(self.chosen)>=le:
                 self.chosen=[]
-                chs = random.randint(0, self.length-1)
+                chs = random.randint(0, le-1)
             else:
                 while chs in self.chosen:
-                    chs = random.randint(0, self.length-1)
+                    chs = random.randint(0, le-1)
             self.chosen.append(chs)
+
         if showName:
-            ch = self.names[0][chs]
+            ch = tar[chs]
         else:
-            ch = self.names[2][chs]
+            ch = self.names[2][self.names[0].index(tar[chs])]
         name.config(text=ch)
 
     def opencfg(self):
@@ -58,17 +75,24 @@ class App(tkinter.Tk):
         button.place(x=300, y=50, anchor="center")
         confb = ttk.Button(self, text="点击打开配置菜单", command=self.opencfg)
         confb.place(x=300, y=150, anchor="center")
+        sexpref = ttk.OptionMenu(self,pref[0],"男女都抽","只抽男","只抽女","男女都抽")
+        sexpref.place(x=300,y=100,anchor="center")
 
     def loadname(self):
-        try:
-            name = pd.read_csv("names.csv",sep=",",header=0,dtype={'name': str, 'sex': int, "no":int})
-            name = name.to_dict()
-            self.names.append(name["name"])
-            self.names.append(name["sex"])
-            self.names.append(name["no"])
-            self.length =len(name["name"])
-        except:
-            print("err")
+        name = pd.read_csv("names.csv",sep=",",header=0,dtype={'name': str, 'sex': int, "no":int})
+        name = name.to_dict()
+        self.names.append(list(name["name"].values()))
+        self.names.append(list(name["sex"].values()))
+        self.names.append(list(name["no"].values()))
+        self.length =len(name["name"])
+        self.sexlen[0] = self.names[1].count(0)
+        self.sexlen[1] = self.names[1].count(1)
+        for i in self.names[0]:
+            if self.names[1][self.names[0].index(i)] == 0:
+                self.sexl[0].append(i)
+            else:
+                self.sexl[1].append(i)
+
     def loadcfg(self):
         try:
             global allowRepeat,alwaysOnTop,showName
