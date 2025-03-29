4b889c56c05fe7ecb199fb28883fa75d962e3a9f
LHGS_github <340994706@qq.com>
2025-03-29 16:35:33 +0800

    feat:翻新配置界面

--- configgui.py
+++ configgui.py
@@ -14,7 +14,6 @@ class cfgpage(tkinter.Toplevel):
         super().__init__()
         self.geometry("450x320")
         self.title("NamePicker - 配置菜单")
-        self.resizable(False, False)
         sv_ttk.set_theme(darkdetect.theme())
         cfgvar = [tkinter.IntVar(), tkinter.IntVar(),tkinter.IntVar(),tkinter.IntVar()]
         self.loadcfg()
@@ -33,14 +32,30 @@ class cfgpage(tkinter.Toplevel):
         res = showinfo("完成","更改已保存，进行一次抽选以应用更改")
 
     def createWidget(self):
+        pages = ttk.Notebook(self)
+        general = ttk.Frame(pages)
+        pages.add(general,text="常规")
+        about = ttk.Frame(pages)
+        pages.add(about,text="关于")
 
-        cfg = [ttk.Checkbutton(self, text="允许重复点名", variable=cfgvar[0]),
-               ttk.Checkbutton(self, text="始终置顶", variable=cfgvar[1]),
-               ttk.Checkbutton(self, text="课表软件支持\n启用后不会在主页显示抽选结果，需要安装CW/CI侧插件", variable=cfgvar[2]),
-               ttk.Label(self,text="当前版本：%s - Codename %s"%(VERSION,CODENAME)),
-               ttk.Button(self, text="保存配置", command=self.savecfg)]
+        cfg = [ttk.Checkbutton(general, text="允许重复点名", variable=cfgvar[0]),
+               ttk.Checkbutton(general, text="始终置顶", variable=cfgvar[1]),
+               ttk.Checkbutton(general, text="课表软件支持\n启用后不会在主页显示抽选结果，需要安装CW/CI侧插件", variable=cfgvar[2]),
+               ]
         for i in range(len(cfg)):
-            cfg[i].place(x=50, y=30 + 50 * i)
+            cfg[i].place(relx=0.1, rely=0.1+0.2*i)
+
+        prod = ttk.Label(about,text="NamePicker - 一款简洁的点名器")
+        prod.place(relx=0.1,rely=0.1)
+        ver = ttk.Label(about, text="当前版本：%s - Codename %s" % (VERSION, CODENAME))
+        ver.place(relx=0.1,rely=0.3)
+        aut = ttk.Label(about, text="作者：灵魂歌手er（GitHub @LHGS-github\n本软件完全开源免费，并遵循MIT协议")
+        aut.place(relx=0.1, rely=0.5)
+
+        save = ttk.Button(self, text="保存配置", command=self.savecfg)
+        save.place(relx=0.4,rely=0.8)
+
+        pages.pack(expand=True, fill="both")
 
     def setcfg(self,t,param):
         if param:
